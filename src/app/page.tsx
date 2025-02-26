60706627b13e4c1150859a4026c02276429810b7
Alvin2805 <alvinsahroni@gmail.com>
2025-02-26 11:47:18 +0700

    move login to the default page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-import RootLayout from "../layout"
+import RootLayout from "./layout"
 
 const Home = () => {
   const router = useRouter()
