be9ce2bfe56d2bd5c1342f34cdf76b95d7bee675
Alvin2805 <alvinsahroni@gmail.com>
2025-03-02 09:31:51 +0700

    added date and time picker

--- src/app/pages/schedule/page.tsx
+++ src/app/pages/schedule/page.tsx
@@ -1,6 +1,9 @@
+'use client'
 import MySidebar from "@/app/components/sidebar"
 import { TextField } from "@mui/material"
 import Grid from "@mui/material/Grid"
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker, LocalizationProvider, TimePicker } from "@mui/x-date-pickers"
 
 const Schedule = () => {
     return (
@@ -10,11 +13,32 @@ const Schedule = () => {
             <Grid
                 container
                 >
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                        label='Pick Date'
+                        sx={{
+                            marginTop:"20px"
+                        }}
+                    >
+                    </DatePicker>
+                </LocalizationProvider>
+                
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <TimePicker
+                        label='Pick Time'
+                        sx={{
+                            marginTop:"20px",
+                            marginLeft:"20px"
+                        }}
+                    >
+                    </TimePicker>
+                </LocalizationProvider>
+                
                 <TextField
                     id="consultation"
                     label="Konsultasi"
                     multiline
-                    maxRows={100}
+                    minRows={10}
                     fullWidth
                     sx={{
                         marginTop:'20px'
